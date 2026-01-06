12d66cc0a3591479b34b945248f076e22047210c
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-07 00:34:43 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// function Increment (action){
+//     var  count = document.getElementById('count')
+
+//     if(action == 'inc'){
+//         count = count.innerText++
+//     }else if(action == 'dec'){
+//         count = count.innerText--
+//     }else if(action == 'res')
+//     {
+//         count = count.innerText = 0
+//     }else
+//     {
+//         alert('Please enter valid number')
+//     }
+    
+// }
+
+
+// function showPassword(){
+//     var show = document.getElementById('input');
+    
+//     if(show.type == 'password'){
+//         show.type = 'text'
+//     }else
+//     {
+//         show.type = 'password'
+//     }
+// }
+
+
+function checkNum (){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
